9e2048de4d938a9f957aaf3bb3e0f4a2c64d6bed
jafestro <jafestro@gmail.com>
2024-09-30 20:03:56 +0300

    first commit

---
+++ src/main/java/com/example/Activity.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Activity {
+    String name;
+    int minutes;
+
+    Activity(String name, int minutes) {
+        this.name = name;
+        this.minutes = minutes;
+    }
+}
---
+++ src/main/java/com/example/SportsTracker.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import java.util.ArrayList;
+
+public class SportsTracker {
+    private final ArrayList<Activity> activities = new ArrayList<>();
+
+    // Method to add a new activity
+    public void logActivity(String name, int minutes) {
+        activities.add(new Activity(name, minutes));
+        System.out.println("Activity logged: " + name + " for " + minutes + " minutes.");
+    }
+
+    // Method to display all activities
+    public void viewActivities() {
+        if (activities.isEmpty()) {
+            System.out.println("No activities logged yet.");
+        } else {
+            System.out.println("Logged activities:");
+            for (Activity activity : activities) {
+                System.out.println(activity.name + " - " + activity.minutes + " minutes");
+            }
+        }
+    }
+
+    // Method to calculate total time spent on sports
+    public void calculateTotalTime() {
+        int totalMinutes = 0;
+        for (Activity activity : activities) {
+            totalMinutes += activity.minutes;
+        }
+        System.out.println("Total time spent on sports this week: " + totalMinutes + " minutes.");
+    }
+}
